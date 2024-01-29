633dc6175f8d9e0bf4086e2c55954f6478824643
Hoai Linh <hoailinh.nguyen@estuary.solutions>
2024-01-29 17:14:13 +0700

    fix: refactor middlewae verify token

--- middlewares/VerifyToken.middleware.js
+++ middlewares/VerifyToken.middleware.js
@@ -3,13 +3,25 @@ const { models } = require("../models");
 const ApiError = require("../utils/ApiError");
 const { decodeToken } = require("../utils/Jwt");
 const HandlerResponseMiddleware = require("./HandlerResponse.middleware");
-const { User } = models;
+const { User, Permission, Role } = models;
 module.exports = async function (req, res, next) {
   try {
-    let auth = req.headers.authorization;
-    auth = auth.slice(7);
-    const payload = await decodeToken(auth, env.SERVER.SERECT_KEY);
-    console.log(payload);
+    let token = req.headers.authorization;
+    token = auth.slice(7);
+
+    await checkToken(token);
+
+    next();
+  } catch (error) {
+    if (error.name === "TokenExpiredError")
+      HandlerResponseMiddleware(new ApiError({ message: "Token Expired", status: "401" }), req, res, next);
+    else HandlerResponseMiddleware(new ApiError({ message: "Token invalid", status: "401" }), req, res, next);
+  }
+};
+
+async function checkToken(token) {
+  try {
+    const payload = await decodeToken(token, env.SERVER.SERECT_KEY);
 
     const instance = await User.findByPk(payload.id);
 
@@ -20,12 +32,20 @@ module.exports = async function (req, res, next) {
 
     // Không cho phép đăng nhập 1 tài khoản trên nhiều thiết bị
     if (instance.token !== auth) throw new ApiError({ message: "Require auth", status: "401" });
-    res.locals.account = instance;
-    next();
+    return instance;
   } catch (error) {
     console.log(error);
-    if (error.name === "TokenExpiredError")
-      HandlerResponseMiddleware(new ApiError({ message: "Token Expired", status: "401" }), req, res, next);
-    else HandlerResponseMiddleware(new ApiError({ message: "Token invalid", status: "401" }), req, res, next);
+    if (error.name === "TokenExpiredError") throw new ApiError({ message: "Token Expired", status: "401" });
+    throw new ApiError({ message: "Token invalid", status: "401" });
   }
-};
+}
+
+async function checkPermission(instance) {
+  try {
+    const permission = await Permission.findOne({ where: { url }, include: { model: Role, as: "roles" } });
+    if (!permission.roles.includes((e) => e.id === instance.idRole))
+      throw new ApiError({ code: "403", message: "Not permission", status: 403 });
+  } catch (error) {
+    throw error;
+  }
+}
